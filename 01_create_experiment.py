d67749ada3db2aef7edbf2b001cfa6825c5270b6
Bloom Verga <bloomify@vivaldi.net>
2024-02-05 10:32:12 +0100

    Create ml flow experiment

---
+++ 01_create_experiment.py
@@ -0,0 +1,12 @@
+import mlflow
+from mlflow_utils import create_mlflow_experiment
+
+if __name__ == "__main__":
+  #create a new mlflow experiment
+  mlflow.create_experiment(
+    name="testing_mlflow1",
+    artifact_location="testing_mlflow1_artifacts",
+    tags={"env": "dev", "version": "1.0.0"},
+  )
+
+  

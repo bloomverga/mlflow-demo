6f9a54ea8a02b36c29a4d104fa98facecda27573
Bloom Verga <bloomify@vivaldi.net>
2024-02-05 11:00:52 +0100

    Retrieve ml flow experiment

--- mlflow_utils.py
+++ mlflow_utils.py
@@ -12,3 +12,22 @@ def create_mlflow_experiment(experiment_name: str, artifact_location: str, tags:
     experiment_id = mlflow.get_experiment_by_name(experiment_name).experiment_id
 
   return experiment_id
+
+def get_mlflow_experiment(experiment_id:str=None, experiment_name:str=None) -> mlflow.entities.Experiment:
+  """
+  Retrieve the mlflow experiment with the given id or name.
+
+  Parameters:
+  ----------
+  experiment_id: str
+    The id of the experiment to retrieve.
+  experiment_name: str
+    The name of the experiment to retrieve.
+  """
+  if experiment_id is not None:
+    experiment = mlflow.get_experiment(experiment_id)
+  elif experiment_name is not None:
+    experiment = mlflow.get_experiment_by_name(experiment_name)
+  else:
+    raise ValueError("Either experiment_id or experiment_name must be provided.")
+  return experiment

7593db4a534663527cd5884f54c3dfb852ce4aeb
Bloom Verga <bloomify@vivaldi.net>
2024-02-05 10:33:20 +0100

    Encapsulate experiment creation into a function

--- 01_create_experiment.py
+++ 01_create_experiment.py
@@ -2,11 +2,12 @@ import mlflow
 from mlflow_utils import create_mlflow_experiment
 
 if __name__ == "__main__":
-  #create a new mlflow experiment
-  mlflow.create_experiment(
-    name="testing_mlflow1",
-    artifact_location="testing_mlflow1_artifacts",
-    tags={"env": "dev", "version": "1.0.0"},
-  )
+  # create a new mlflow experiment
+  # mlflow.create_experiment(
+  #   name="testing_mlflow1",
+  #   artifact_location="testing_mlflow1_artifacts",
+  #   tags={"env": "dev", "version": "1.0.0"},
+  # )
 
-  
+  experiment_id = create_mlflow_experiment(experiment_name="testing_mlflow2", artifact_location="testing_mlflow1_artifacts", tags={"env": "dev", "version": "1.0.0"})
+  print(f"Experiment ID: ", experiment_id)
